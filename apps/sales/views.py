d841ac7d0182d8f1cbe2e9738183dd0bb9dd8db4
Armel Bogue <komlanbogue@gmail.com>
2026-01-28 14:24:13 +0000

    feat: Override create method in CommandeViewSet to remove client from request data

--- apps/sales/views.py
+++ apps/sales/views.py
@@ -45,6 +45,16 @@ class CommandeViewSet(viewsets.ModelViewSet):
         """Set the client to the current user for security."""
         serializer.save(client=self.request.user)
     
+    def create(self, request, *args, **kwargs):
+        """Override create to remove client from data since it's read-only."""
+        data = request.data.copy()
+        data.pop('client', None)
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
     
     @action(detail=True, methods=['post'])
     def assign(self, request, pk=None):
